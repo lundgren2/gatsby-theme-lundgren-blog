8b3a61a2bd75b4847a94b17cd2c16004692395de
Tobias Lundgren <tobias@lundgren.design>
2019-03-29 17:48:21 +0100

    Add social buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,50 +2,23 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 import { Flex, Text } from '@rebass/emotion';
-import website from '../../config/website';
 import Container from './Container';
-import Icon from './Icon';
+import website from '../../config/website';
 
 const FooterColumn = styled(Flex)``;
-const FooterListItem = ({ Icon: IconComp, rel, children, href }) => (
-  <Text as="div" my={1} color="#666">
-    <a href={href} rel={rel}>
-      {IconComp && (
-        <Icon mr={2} ml={0}>
-          <IconComp size="1em" />
-        </Icon>
-      )}
-      {children}
-    </a>
-  </Text>
-);
 
 export default () => (
-  <Flex
-    py={5}
-    mt={5}
-    as="footer"
-    // bg="#f9f9f9"
-    css={css`
-      border-top: 1px solid rgba(0, 0, 0, 0.1);
-    `}
-  >
+  <Flex py={5} mt={5} as="footer">
     <Container width={1} css={{ maxWidth: 1440 }}>
       <Flex flexDirection={['column', 'row']}>
         <FooterColumn
-          title="About this place"
-          width={[1, 1 / 2]}
+          // width={[1, 0.5]}
           flexDirection="column"
           flex={1}
           mb={4}
           mr={4}
         >
-          <Text color="#666" lineHeight={1.5}>
-            © MMXIX {website.siteTitle}
-          </Text>
-        </FooterColumn>
-        <FooterColumn title="About this place" width={[1, 1 / 2]}>
-          <Text color="#666" lineHeight={1.5}>
+          <Text color="#888" lineHeight={1.5}>
             © MMXIX {website.siteTitle}
           </Text>
         </FooterColumn>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import styled from '@emotion/styled';
+import { Twitter, GitHub, Rss } from 'react-feather';
 import media from '../utils/media';
 
 const SidebarWrapper = styled('div')`
@@ -8,10 +10,9 @@ const SidebarWrapper = styled('div')`
   position: absolute;
   display: none;
   right: 40px;
-  top: 60px;
+  top: 80px;
   width: 27px;
   height: 27px;
-  font-weight: 500;
   font-size: 16px;
   white-space: nowrap;
   pointer-events: none;
@@ -23,8 +24,46 @@ const SidebarWrapper = styled('div')`
     display: block;
   }
 `;
-const Sidebar = () => {
-  return <SidebarWrapper>Tobias Lundgren</SidebarWrapper>;
-};
+
+const SocialWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  flex-direction: row;
+  right: 40px;
+  bottom: 5%;
+  a svg {
+    margin-left: 8px;
+    color: ${props => props.theme.colors.text};
+    &:hover {
+      opacity: 0.6;
+    }
+  }
+
+  ${media.sm} {
+    flex-direction: column;
+    right: 40px;
+    bottom: 40px;
+    a svg {
+      margin-top: 8px;
+    }
+  }
+`;
+const Sidebar = () => (
+  <>
+    <SidebarWrapper>Tobias Lundgren</SidebarWrapper>,
+    <SocialWrapper>
+      <a href="https://github.com/lundgren2" rel="me" target="_blank">
+        <GitHub size="1.65em" />
+      </a>
+      <a href="https://twitter.com/lundgren2" rel="me" target="_blank">
+        <Twitter size="1.65em" />
+      </a>
+      <Link to="/">
+        <Rss size="1.65em" />
+      </Link>
+    </SocialWrapper>
+  </>
+);
 
 export default Sidebar;
